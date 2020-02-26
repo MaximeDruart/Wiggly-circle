7afbe9fc921775322f7fa98ae628fad67da453d8
Maxime Druart <maxime.druart@yahoo.fr>
2020-02-26 15:04:22 +0100

    Updates

---
+++ precache-manifest.fc2777aa5328bdf06b5a90d4baf0d9fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640ff8ec77bc688355e1189dd7f58709",
+    "url": "/wiggly-circle/index.html"
+  },
+  {
+    "revision": "3d4474b2c8d2f48fa6fd",
+    "url": "/wiggly-circle/static/css/main.28c48bb8.chunk.css"
+  },
+  {
+    "revision": "432b001060caa1775c71",
+    "url": "/wiggly-circle/static/js/2.e087e6a4.chunk.js"
+  },
+  {
+    "revision": "33f61a41057747c933779a93eb46f6ce",
+    "url": "/wiggly-circle/static/js/2.e087e6a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d4474b2c8d2f48fa6fd",
+    "url": "/wiggly-circle/static/js/main.9a402e9a.chunk.js"
+  },
+  {
+    "revision": "5c3081d20de8399e5513",
+    "url": "/wiggly-circle/static/js/runtime-main.130ee72e.js"
+  }
+]);
